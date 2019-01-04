784e7f3d7671ffe4299726f431c5dbaa66223fbe
Tomasz  Boczarski <tomasz.boczarski@comarch.pl>
2019-01-04 14:29:06 +0100

    fix invalid registration

--- src/register.js
+++ src/register.js
@@ -5,18 +5,27 @@ const storage = require('./storage');
 router.route('/').post(registrationHandler);
 
 async function registrationHandler(req, res) {
-    const data = await storage.get();
-    const userExist = data.users.includes( (item) => item.username === req.body.username );
-    if(userExist) {
-        res.sendStatus(403).send({message: "User already exist"});
+    if(!isRegistrationDataValid(req.body)) {
+        res.sendStatus(403).send({mesage: "Invalid data"});
     }
     else {
-        const user = {};
-        user.username = req.body.username;
-        user.password = req.body.password;
-        data.users.push(user);
-        res.sendStatus(await storage.set(data));
+        const data = await storage.get();
+        const userExist = data.users.includes( (item) => item.username === req.body.username );
+        if(userExist) {
+            res.sendStatus(403).send({message: "User already exist"});
+        }
+        else {
+            const user = {};
+            user.username = req.body.username;
+            user.password = req.body.password;
+            data.users.push(user);
+            res.sendStatus(await storage.set(data));
+        }
     }
+}
+
+function isRegistrationDataValid(data) {
+    return data.username && data.password
 }
 
 module.exports = router;
